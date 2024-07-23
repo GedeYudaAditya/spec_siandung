bfbb2d521e94bfc303a2f9cc6ddfe45f40759802
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2024-07-23 08:56:04 +0800

    Perubahan Di Log

--- lib/src/screens/log_screen.dart
+++ lib/src/screens/log_screen.dart
@@ -28,22 +28,77 @@ class _LogScreenState extends State<LogScreen> {
             future: apiService.fetchData('log?id_laporan=' + args.id!),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
+                return Center(child: const CircularProgressIndicator());
               } else {
                 if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
                 } else {
                   List<Log> logs =
                       Log.fromJsonList(snapshot.data!['data']['log']);
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: logs.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(logs[index].status),
-                        subtitle: Text(logs[index].keterangan),
-                      );
-                    },
+                  return Column(
+                    children: [
+                      Container(
+                        padding:
+                            const EdgeInsets.only(top: 16, right: 16, left: 16),
+                        alignment: Alignment.centerLeft,
+                        child: Text('${snapshot.data!['data']['klasifikasi']}',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(
+                            right: 16, left: 16, top: 10, bottom: 5),
+                        alignment: Alignment.centerLeft,
+                        child: Text('Detail Laporan:',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            textAlign: TextAlign.left),
+                      ),
+                      // text paragraph dari laporan
+                      Container(
+                        child: SingleChildScrollView(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                                snapshot.data!['data']['keterangan'] ?? '',
+                                style: TextStyle(fontSize: 16, height: 1.5),
+                                textAlign: TextAlign.justify),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(
+                            right: 16, left: 16, top: 10, bottom: 5),
+                        alignment: Alignment.centerLeft,
+                        child: Text('Log List:',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            textAlign: TextAlign.left),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: logs.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: ListTile(
+                                title: Text(logs[index].status),
+                                subtitle: Text(logs[index].keterangan),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   );
                 }
               }
